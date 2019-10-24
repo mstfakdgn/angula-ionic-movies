5eb34a8b16dccd366b39d6b6216dd3a4cc404853
mstfakdgn <mustafa/akiler@gmail.com>
2019-10-24 12:39:54 +0300

    detail finished

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
-
-  constructor() { }
+  loadedItem: any;
+  isLoading = false;
+  constructor(private activatedRoute: ActivatedRoute, private service: MovieService) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.isLoading = true;
+    this.service.getDetails(id).subscribe(result => {
+      console.log(result);
+      this.loadedItem = result;
+      this.isLoading = false;
+    });
+  }
+
+  openWebsite() {
+    window.open(this.loadedItem.Website, '_blank');
   }
 
 }
